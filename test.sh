0cea6ab4c7f7638f34c230fd1ec287c102aa80c8
Martin Rybak <martin.rybak@gmail.com>
2024-05-17 11:45:43 -0700

    Added test.sh for generating code coverage reports and included output

---
+++ test.sh
@@ -0,0 +1,11 @@
+# Runs all tests and output code coverage to coverage/ folder
+
+# Generate `coverage/lcov.info` file
+flutter test --coverage
+
+# Generate HTML report
+# Note: on macOS you need to have lcov installed on your system (`brew install lcov`) to use this:
+genhtml coverage/lcov.info -o coverage/html
+
+# Open the report
+open coverage/html/index.html
